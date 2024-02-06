e6c376fbb009df69d442e63b44ab16a706ab9e0b
LAB2-PC28 <LAB2-PC28@DESKTOP-LEL3KMU>
2024-02-05 21:54:48 -0300

    encapsulamento

--- AULADOIS/src/aula2/Cachorro.java
+++ AULADOIS/src/aula2/Cachorro.java
@@ -1,15 +1,43 @@
 package aula2;
 
 public class Cachorro {
-	public String nome;
-	public String raca;
-	public int idade;
-	public String cor;
-	public String desc;
+	private String nome;
+	private String raca;
+	private int idade;
+	private String cor;
+	private String desc;
 	
+	public String Getnome() {
+	return nome;
+	}
+	public String Getraca() {
+	return raca;
+	}
+	public int Getidade() {
+	return idade;
+	}
+	public String Getcor() {
+	return cor;
+	}
+	public String Getdesc() {
+	return desc;
+	}
 	
-	
-	
+	public void Setnome(String nome) {
+	this.nome = nome;
+	}
+	public void Setraca(String raca) {
+	this.raca = raca;
+	}
+	public void Setidade(int idade) {
+	this.idade = idade;
+	}
+	public void Setcor(String cor) {
+	this.cor = cor;
+	}
+	public void Setdesc(String desc) {
+	this.desc = desc;
+}
 public void latir () {
 	System.out.println("O cachorro " + nome + "latiu");
 }
